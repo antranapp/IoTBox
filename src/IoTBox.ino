3c28b963da7fd55d75b56ca73e44d30e03023926
Binh An Tran <tran.binhan@gmail.com>
2017-10-16 21:33:00 +0200

    FW-13, FW-27, FW28: Add some sound for open, close, configuration events

--- src/IoTBox.ino
+++ src/IoTBox.ino
@@ -19,6 +19,100 @@
 #define SWITCHPIN D6 // Switch for "LOCKED" or "OPEN" status.
 #define LEDPIN D7 // Status LED.
 
+/*************************************************
+ * Public Constants
+ *************************************************/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
 unsigned long lastSync = millis();
 
 rgb_lcd lcd;
@@ -30,6 +124,46 @@ volatile bool requestDisplayConfigurationTime = true;
 volatile bool requestOpeningOperation = false;
 volatile bool isLCDBusy = false;
 
+
+int melodyOpen[] = {
+    NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+    NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+    NOTE_A4, NOTE_G4, NOTE_A4, 0,
+};
+int noteDurationsOpen[] = {
+    4, 4, 8, 4, 4,
+    4, 4, 8, 4, 4,
+    4, 4, 8, 4, 4,
+    4, 4, 12, 4,
+};
+
+int melodyClose[] = {
+    NOTE_G3, NOTE_C3, NOTE_DS3, NOTE_F3,
+    NOTE_G3 ,NOTE_C3, NOTE_DS3, NOTE_F3,
+    NOTE_D3
+};
+int noteDurationsClose[] = {
+    12, 12, 4, 4,
+    8, 8, 4, 4,
+    12,
+};
+
+int melodyConfiguration[] = {
+    NOTE_E7, NOTE_E7, 0, NOTE_E7,
+    0, NOTE_C7, NOTE_E7, 0,
+    NOTE_G7, 0, 0,  0,
+    NOTE_G6, 0, 0, 0,
+    NOTE_C7,
+};
+int noteDurationsConfiguration[] = {
+    12, 12, 12, 12,
+    12, 12, 12, 12,
+    12, 12, 12, 12,
+    12, 12, 12, 12,
+    12,
+};
+
 class OpenOperation {
   public:
 
@@ -38,15 +172,10 @@ class OpenOperation {
     }
 
     void start() {
-        // TODO: Check if box is arelady open
         // TODO: Delay 2 seconds after 2 consecutive open operations
         if (!isRunning()) {
             // Only open the box when it is closed
             if (switchState == HIGH) {
-                lcd.setCursor(0,1);
-                lcd.print("               ");
-                lcd.setCursor(0,1);
-                lcd.print("opening....");
                 digitalWrite(RELAYPIN, HIGH);
                 _timer->reset();
             }
@@ -88,6 +217,8 @@ struct ConfigurationTime {
 
 ConfigurationTime configurationTime;
 
+const char *PUBLISH_EVENT_NAME = "status";
+
 void setup() {
     // Ouput pins
     pinMode(LEDPIN, OUTPUT);
@@ -110,10 +241,14 @@ void setup() {
     Time.zone(+2.00);
 
     // Register the status of the box to Particle Cloud
+    switchState = digitalRead(SWITCHPIN);
     Particle.variable("switchState", switchState);
 
     // Register the configureTime as a cloud function
     Particle.function("time", configureTime);
+
+    // Register the startOpenOperation as a cloud function
+    Particle.function("open", startOpenOperation);
 
     // Get the configured time from EEPROM, set it to default if not saved
     EEPROM.get(0, configurationTime);
@@ -121,6 +256,8 @@ void setup() {
         // EEPROM was empty -> initialize myObj
         configurationTime = { 0, 7, 0 };
     }
+
+    Serial.begin(9600);
 
     clockTimer.start();
 }
@@ -130,8 +267,8 @@ void loop() {
 
     if (requestOpeningOperation) {
         digitalWrite(LEDPIN, HIGH);
+        startBuzzerForOpenOperation();
         openOperation.start();
-
         requestOpeningOperation = false;
     } else {
         digitalWrite(LEDPIN, LOW);
@@ -149,15 +286,51 @@ void syncTimeWithCloudIfNeeded() {
     }
 }
 
+void startBuzzerForOpenOperation() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        int noteDuration = 1000/noteDurationsOpen[thisNote];
+        tone(BUZZERPIN, melodyOpen[thisNote], noteDuration);
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        noTone(BUZZERPIN);
+    }
+}
+
+void startBuzzerForClose() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        int noteDuration = 1000/noteDurationsClose[thisNote];
+        tone(BUZZERPIN, melodyClose[thisNote], noteDuration);
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        noTone(BUZZERPIN);
+    }
+}
+
+void startBuzzerForConfiguration() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        int noteDuration = 1000/noteDurationsConfiguration[thisNote];
+        tone(BUZZERPIN, melodyConfiguration[thisNote], noteDuration);
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        noTone(BUZZERPIN);
+    }
+}
+
 void displayStatus() {
     if (requestDisplayStatus) {
+
+        // Publish the status to Particle Cloud
+        publishData();
+
         if (!isLCDBusy) {
             isLCDBusy = true;
             if (switchState == HIGH) {
                 lcd.setCursor(0,1);
                 lcd.print("               ");
                 lcd.setCursor(0,1);
-                lcd.print("LOCKED");
+                lcd.print("CLOSED");
+
+                startBuzzerForClose();
             } else {
                 lcd.setCursor(0,1);
                 lcd.print("               ");
@@ -176,7 +349,7 @@ void updateTime() {
     minute = Time.minute();
     second = Time.second();
 
-    requestDisplayTime = true;
+    //requestDisplayTime = true;
 
     checkOpenCondition();
 }
@@ -201,6 +374,9 @@ void displayTime() {
 
 void displayConfigurationTime() {
     if (requestDisplayConfigurationTime) {
+
+        startBuzzerForConfiguration();
+
         if (!isLCDBusy) {
             isLCDBusy = true;
             lcd.setCursor(11,0);
@@ -235,7 +411,12 @@ void updateSwitchState() {
 }
 
 void updateButtonState() {
+    startOpenOperation("");
+}
+
+int startOpenOperation(String command) {
     requestOpeningOperation = true;
+    return 1;
 }
 
 void checkOpenCondition() {
@@ -266,4 +447,10 @@ int configureTime(String time)
         return 1;
     }
     return -1;
+}
+
+void publishData() {
+	char buf[256];
+	sprintf(buf, "{\"status\":%d,\"deviceID\":\"%s\"}", switchState, System.deviceID().c_str());
+	Particle.publish(PUBLISH_EVENT_NAME, buf, PRIVATE);
 }
